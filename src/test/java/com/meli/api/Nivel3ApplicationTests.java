2fa044f55ebfa904cc368408dd836dd83d06fb2b
Roberto Capone <roberto.capone@enerminds.com>
2020-06-28 16:53:19 -0300

    [STEP 3] Create GIT repository

---
+++ src/test/java/com/meli/api/Nivel3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nivel3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
